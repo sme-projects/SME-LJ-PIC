2280f535c02b255b201af42ce77130fc0a74590c
Alberte Thegler <alberte@thegler.dk>
2020-04-02 09:17:56 +0200

    Added position_update to the circuit

--- Lennard_Jones/Program.cs
+++ Lennard_Jones/Program.cs
@@ -1,5 +1,6 @@
 ﻿using SME;
 using SME.Components;
+using System;
 using Deflib;
 using Acceleration;
 using Velocity_Update;
@@ -12,7 +13,7 @@ namespace Lennard_Jones
         {
             using(var sim = new Simulation())
             {
-                uint data_size = 28;
+                uint data_size = 20;
                 float timestep_size = 10.0f;
 
                 // RAM
@@ -26,10 +27,15 @@ namespace Lennard_Jones
                 // Managers
                 var acceleration_manager = new Acceleration.Manager();
                 var velocity_manager = new Velocity_Update.Manager(data_size, timestep_size);
+                var position_manager = new Position_Update.Manager(data_size, timestep_size);
                 
                 // Multiplexers
                 var init_pos_data_multiplexer = new Multiplexer_ControlA();
-                var velocity_data_multiplexer = new Multiplexer_ControlB();
+                var pos_data_multiplexer = new Multiplexer_ControlA();
+                var velocity_data_multiplexer = new Multiplexer_ControlB(); 
+                var position_update_multiplexer = new Multiplexer_ControlB(); 
+                var velocity_update_multiplexer = new Multiplexer_ControlA(); 
+
 
                 // Cache
                 var acceleration_cache = new Cache.AccelerationCache((uint)Cache_size.n);
@@ -43,14 +49,27 @@ namespace Lennard_Jones
                     new Velocity_Update.Update_velocity(velocity_manager.prev_velocity, 
                                 velocity_manager.acceleration_data_point,timestep_size);
 
+                // Position class
+                Position_Update.Update_position position = 
+                    new Position_Update.Update_position(position_manager.prev_position, 
+                                position_manager.velocity_data_point,timestep_size);
+
                 // Connections
-                external_simulator.pos_ramctrl = init_pos_data_multiplexer.first_input;
+                external_simulator.position_ramctrl = init_pos_data_multiplexer.first_input;
                 acceleration_manager.pos1_ramctrl = init_pos_data_multiplexer.second_input;
 
-                init_pos_data_multiplexer.output = position_ram.ControlA;
+                init_pos_data_multiplexer.output = pos_data_multiplexer.first_input;
+                position_manager.position_ramctrl = pos_data_multiplexer.second_input;
+                pos_data_multiplexer.output = position_ram.ControlA;
+                
 
-                acceleration_manager.pos2_ramctrl = position_ram.ControlB;
+                acceleration_manager.pos2_ramctrl = position_update_multiplexer.first_input;
+                position_manager.updated_position_ramctrl = position_update_multiplexer.second_input;
+                position_update_multiplexer.output = position_ram.ControlB;
+
                 acceleration_manager.pos1_ramresult = position_ram.ReadResultA;
+                external_simulator.position_ramresult = position_ram.ReadResultA;
+                position_manager.position_ramresult = position_ram.ReadResultA;
                 acceleration_manager.pos2_ramresult = position_ram.ReadResultB;
 
                 acceleration_manager.ready = external_simulator.acc_ready;
@@ -66,20 +85,31 @@ namespace Lennard_Jones
 
                 velocity_manager.updated_velocity = velocity.updated_data_point;
 
-                velocity_manager.velocity_ramctrl = velocity_ram.ControlA;
-                velocity_manager.velocity_ramresult = velocity_ram.ReadResultA;
-
-                velocity_manager.updated_velocity_ramctrl = velocity_data_multiplexer.second_input;
-                external_simulator.velocity_ramctrl = velocity_data_multiplexer.first_input;
+                velocity_manager.velocity_ramctrl = velocity_data_multiplexer.first_input;
+                position_manager.velocity_data_point_ramctrl = velocity_data_multiplexer.second_input;
+                
                 velocity_data_multiplexer.output = velocity_ram.ControlB;
+                velocity_manager.velocity_ramresult = velocity_ram.ReadResultB;
+ 
+                velocity_manager.updated_velocity_ramctrl = velocity_update_multiplexer.second_input;
+                external_simulator.init_velocity_ramctrl = velocity_update_multiplexer.first_input;
+                velocity_update_multiplexer.output = velocity_ram.ControlA;
 
-                external_simulator.velocity_ramresult = velocity_ram.ReadResultB;
+                position_manager.velocity_data_point_ramresult = velocity_ram.ReadResultB;
 
-                velocity_manager.sim_ready = external_simulator.sim_ready_velocity;
-                external_simulator.result_ready = velocity_manager.finished;
+                velocity_manager.sim_ready = external_simulator.velocity_reset; 
+                position_manager.data_ready = velocity_manager.finished;
+
+                position_manager.reset = external_simulator.position_reset;
+                position_manager.updated_position = position.updated_data_point;
+                position.prev_data_point = position_manager.prev_position;
+                position.velocity_data_point = position_manager.velocity_data_point;
+
+                external_simulator.finished = position_manager.finished;
 
                 // sim.Run(null, () => true);
                 sim.Run();
+                Console.WriteLine("Simulation completed");
             }
         }
     }
--- Lennard_Jones/Simulation.cs
+++ Lennard_Jones/Simulation.cs
@@ -12,22 +12,25 @@ namespace Lennard_Jones
     public class External_Sim : SimulationProcess
     {
         [InputBus]
-        public FlagBus result_ready;
-
+        public FlagBus finished;
+        
         [InputBus]
-        public TrueDualPortMemory<uint>.IReadResultB velocity_ramresult;
+        public TrueDualPortMemory<uint>.IReadResultA position_ramresult;
 
         [OutputBus]
-        public TrueDualPortMemory<uint>.IControlB velocity_ramctrl;
+        public TrueDualPortMemory<uint>.IControlA init_velocity_ramctrl;
     
         [OutputBus]
-        public TrueDualPortMemory<uint>.IControlA pos_ramctrl;
+        public TrueDualPortMemory<uint>.IControlA position_ramctrl;
 
         [OutputBus]
         public ValBus acc_ready = Scope.CreateBus<ValBus>();
 
         [OutputBus]
-        public FlagBus sim_ready_velocity = Scope.CreateBus<FlagBus>();
+        public FlagBus velocity_reset = Scope.CreateBus<FlagBus>();
+        
+        [OutputBus]
+        public FlagBus position_reset = Scope.CreateBus<FlagBus>();
 
         
 
@@ -53,42 +56,45 @@ namespace Lennard_Jones
             // generated correctly
             float[] positions = new float[data_size];
             for(uint i = 0; i < data_size; i++){
-                int random_number = rnd.Next(10,1000);
-                float float_rnd_number = random_number;
-                if(!positions.Contains((float) random_number))
-                    positions[i] = (float) random_number;
-                else
-                    i--;
+                // int random_number = rnd.Next(10,1000);
+                // float float_rnd_number = random_number;
+                // if(!positions.Contains((float) random_number))
+                //     positions[i] = (float) random_number;
+                // else
+                //     i--;
                 // Non-random data:
-                // positions[i] = (float) i + 1;
+                positions[i] = (float) i + 1;
+                // Console.WriteLine("position: {0}", positions[i]);
             }
 
             for(int i = 0; i < positions.Length; i++){
                 // Write initial data to position ram
-                pos_ramctrl.Address = i;
-                pos_ramctrl.Data = Funcs.FromFloat(positions[i]);
-                pos_ramctrl.IsWriting = true;
-                pos_ramctrl.Enabled = true;
+                position_ramctrl.Address = i;
+                position_ramctrl.Data = Funcs.FromFloat(positions[i]);
+                position_ramctrl.IsWriting = true;
+                position_ramctrl.Enabled = true;
 
                 // Write initial data to velocity ram
-                velocity_ramctrl.Address = i;
+                init_velocity_ramctrl.Address = i;
                 // Initial value is 0
-                velocity_ramctrl.Data = Funcs.FromFloat(0.0f); 
-                velocity_ramctrl.IsWriting = true;
-                velocity_ramctrl.Enabled = true;
+                init_velocity_ramctrl.Data = Funcs.FromFloat(0.0f); 
+                init_velocity_ramctrl.IsWriting = true;
+                init_velocity_ramctrl.Enabled = true;
 
                 await ClockAsync();
             }
 
             bool running = true;
-            pos_ramctrl.Enabled = false;
-            velocity_ramctrl.Enabled = false;
+            position_ramctrl.Enabled = false;
+            init_velocity_ramctrl.Enabled = false;
             acc_ready.val = data_size;
             acc_ready.valid = true;
-            sim_ready_velocity.valid = true;
+            velocity_reset.valid = true;
+            position_reset.valid = true;
             await ClockAsync();
             acc_ready.valid = false;
-            sim_ready_velocity.valid = false;
+            velocity_reset.valid = false;
+            position_reset.valid = false;
 
 
             // Calculating data for verifying results
@@ -109,27 +115,34 @@ namespace Lennard_Jones
                 updated_velocity[i] = update_result;
             }
 
+            float[] updated_positions = new float[data_size];
+            // Calculate data for tests
+            for( int i = 0; i < data_size; i++){
+                float update_result = Sim_Funcs.Update_Data_Calc(positions[i], updated_velocity[i], timestep);
+                updated_positions[i] = update_result;
+            }
+
             int k = 0;
             int n = 0;
 
             while(running){
-                if(result_ready.valid){
+                if(finished.valid){
                     
                     if(k < data_size){
-                        velocity_ramctrl.Enabled = true;
-                        velocity_ramctrl.Data = 0;
-                        velocity_ramctrl.IsWriting = false;
-                        velocity_ramctrl.Address = k;
+                        position_ramctrl.Enabled = true;
+                        position_ramctrl.Data = 0;
+                        position_ramctrl.IsWriting = false;
+                        position_ramctrl.Address = k;
                         k++;
                     }else{
-                        velocity_ramctrl.Enabled = false;
+                        position_ramctrl.Enabled = false;
                     }
 
                     if(k-n > 2 || k >= data_size){
-                        float input_result = Funcs.FromUint(velocity_ramresult.Data);
-                        if(Math.Abs(updated_velocity[n] - input_result) > 0.0f)
-                            Console.WriteLine("Update velocity result - Got {0}, expected {1} at {2}",
-                                    input_result, updated_velocity[n], n);
+                        float input_result = Funcs.FromUint(position_ramresult.Data);
+                        if(Math.Abs(updated_positions[n] - input_result) > 0.0f)
+                            Console.WriteLine("Update position result - Got {0}, expected {1} at {2}",
+                                    input_result, updated_positions[n], n);
                         n++;
                     }
                     if(n >= data_size)
